529905b2bf037479e2dc72d4cecdd88872b578d7
Maxime LEBLANC <leblanc.maxime49@hotmail.fr>
2025-06-26 15:01:37 +0200

    Add data and update repositories

--- src/main/java/guru/springframework/spring5_recipe_app/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5_recipe_app/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package guru.springframework.spring5_recipe_app.controllers;
 
+import guru.springframework.spring5_recipe_app.domain.Category;
+import guru.springframework.spring5_recipe_app.domain.UnitOfMeasure;
+import guru.springframework.spring5_recipe_app.repositories.CategoryRepository;
+import guru.springframework.spring5_recipe_app.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        Optional<Category> category = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category Id is:" + category.get().getId());
+        System.out.println("UOM Id is:" + unitOfMeasure.get().getId());
+
         return "index";
     }
 }
