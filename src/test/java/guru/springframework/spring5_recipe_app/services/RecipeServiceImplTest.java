5759e6829f398c7c30cc054f9b8e1fd94304de4b
Maxime LEBLANC <leblanc.maxime49@hotmail.fr>
2025-07-09 10:51:27 +0200

    adding deletion of recipe by id

--- src/test/java/guru/springframework/spring5_recipe_app/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/spring5_recipe_app/services/RecipeServiceImplTest.java
@@ -39,30 +39,46 @@ class RecipeServiceImplTest {
 
     @Test
     void getRecipes() {
+        // Given
         Recipe recipe = new Recipe();
         Set<Recipe> recipesData = new HashSet<>();
         recipesData.add(recipe);
-
         when(recipeRepository.findAll()).thenReturn(recipesData);
 
+        // When
         Set<Recipe> recipes = recipeService.getRecipes();
 
+        // Then
         assertEquals(1, recipes.size());
         verify(recipeRepository, times(1)).findAll();
     }
 
     @Test
     public void getRecipeById() throws Exception {
+        // Given
         Recipe recipe = new Recipe();
         recipe.setId(1L);
         Optional<Recipe> recipeOptional = Optional.of(recipe);
-
         when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
 
+        // When
         Recipe recipeReturned = recipeService.findById(1L);
 
+        // Then
         assertNotNull(recipeReturned);
         verify(recipeRepository, times(1)).findById(anyLong());
         verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
+    void deleteById() {
+        // Given
+        Long idToDelete = 2L;
+
+        // When
+        recipeService.deleteById(idToDelete);
+
+        // Then
+        verify(recipeRepository, times(1)).deleteById(anyLong());
     }
 }
