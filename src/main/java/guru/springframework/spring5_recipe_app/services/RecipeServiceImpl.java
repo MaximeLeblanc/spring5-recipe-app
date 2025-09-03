3554b59d4ef6b0cc792894ade6f4057f1a52777b
Maxime LEBLANC <leblanc.maxime49@hotmail.fr>
2025-09-03 10:48:49 +0200

    Added error information

--- src/main/java/guru/springframework/spring5_recipe_app/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/spring5_recipe_app/services/RecipeServiceImpl.java
@@ -47,7 +47,7 @@ public class RecipeServiceImpl implements RecipeService {
         Optional<Recipe> recipeOptional = recipeRepository.findById(id);
 
         if (recipeOptional.isEmpty()) {
-            throw new NotFoundException("Recipe Not Found!");
+            throw new NotFoundException("Recipe Not Found. For id value " + id);
         }
 
         return recipeOptional.get();
