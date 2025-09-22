d84ce77a47bb58ca740da4cbb56d3c51c57dc2d3
Maxime LEBLANC <leblanc.maxime49@hotmail.fr>
2025-09-22 15:55:17 +0200

    Update for MongoDb

--- src/test/java/guru/springframework/spring5_recipe_app/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/spring5_recipe_app/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,23 +1,37 @@
 package guru.springframework.spring5_recipe_app.repositories;
 
+import guru.springframework.spring5_recipe_app.bootstrap.RecipeBootstrap;
 import guru.springframework.spring5_recipe_app.domain.UnitOfMeasure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DataJpaTest
+@DataMongoTest
 class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    RecipeRepository recipeRepository;
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
     @BeforeEach
     void setUp() {
+        recipeRepository.deleteAll();
+        categoryRepository.deleteAll();
+        unitOfMeasureRepository.deleteAll();
+
+        RecipeBootstrap recipeBootstrap = new RecipeBootstrap(categoryRepository, recipeRepository, unitOfMeasureRepository);
+
+        recipeBootstrap.onApplicationEvent(null);
     }
 
     @Test
