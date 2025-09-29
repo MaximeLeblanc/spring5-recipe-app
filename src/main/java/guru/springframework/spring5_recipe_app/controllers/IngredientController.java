52ef7176fe169714b1b404bf31a4fa51ea828c2f
Maxime LEBLANC <leblanc.maxime49@hotmail.fr>
2025-09-29 16:32:15 +0200

    Fix

--- src/main/java/guru/springframework/spring5_recipe_app/controllers/IngredientController.java
+++ src/main/java/guru/springframework/spring5_recipe_app/controllers/IngredientController.java
@@ -34,7 +34,7 @@ public class IngredientController {
         log.debug("Getting ingredient list for recipe id: " + recipeId);
 
         // use command object to avoid lazy load errors in Thymeleaf.
-        model.addAttribute("recipe", recipeService.findCommandById(recipeId));
+        model.addAttribute("recipe", recipeService.findCommandById(recipeId).block());
 
         return "recipe/ingredient/list";
     }
@@ -50,7 +50,7 @@ public class IngredientController {
     @GetMapping("/recipe/{recipeId}/ingredient/new")
     public String newRecipe(@PathVariable String recipeId, Model model) {
         // Make sure we have a good id value
-        RecipeCommand recipeCommand = recipeService.findCommandById(recipeId);
+        RecipeCommand recipeCommand = recipeService.findCommandById(recipeId).block();
         // TODO: raise exception if null
 
         // Need to return back parent id for hidden form property
